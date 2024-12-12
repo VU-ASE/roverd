353859601746e0314ef65feaa1f47082e7f38073
maxgallup <maxgallup@pm.me>
2024-12-12 01:23:28 +0000

    chore: added zips

--- scripts/setup_rover_files.sh
+++ scripts/setup_rover_files.sh
@@ -21,6 +21,6 @@ echo "bunny" >> /etc/rover
 echo -n "debix" | sha256sum | cut -d ' ' -f 1 >> /etc/rover
 
 
-cp roverd/examples/imaging-service.yaml /home/debix/.rover/vu-ase/imaging/1.0.0/service.yaml
-cp roverd/examples/controller-service.yaml /home/debix/.rover/vu-ase/controller/1.0.1/service.yaml
-cp roverd/examples/actuator-service.yaml /home/debix/.rover/vu-ase/actuator/1.0.5/service.yaml
+# cp roverd/examples/imaging-service.yaml /home/debix/.rover/vu-ase/imaging/1.0.0/service.yaml
+# cp roverd/examples/controller-service.yaml /home/debix/.rover/vu-ase/controller/1.0.1/service.yaml
+# cp roverd/examples/actuator-service.yaml /home/debix/.rover/vu-ase/actuator/1.0.5/service.yaml
